c282965ba44744057dd2b22f8f65b53f7fdd57c9
Neville Jos <njos@Nevilles-MacBook-Pro.local>
2015-06-10 15:38:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+JsonConf [![GoDoc](https://godoc.org/github.com/auroratechnologies/jsonconf?status.svg)](https://godoc.org/github.com/auroratechnologies/jsonconf)
+======
+
+######JsonConf
+
+JsonConf implement a method of reading environment variables and defaulting to a configuration json file if the environment variable is not found.
+
+
+This helps setup a single configuration file that can be used in production, development and local environments, while all having different configurations if the appropriate environment variables are setup.
+
+###Full example
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/auroratechnologies/jsonconf"
+)
+
+
+func main() {
+	jsonconf.LoadConfig("conf.json")
+	redisaddr, err := jsonconf.GetVar("REDIS_ADDR"))
+  if (err != nil){
+    fmt.Println(err) // No key found
+  }
+  fmt.Println(redisaddr)
+}
+```
+
+Full package and usage documentation can be located at [the godocs site](https://godoc.org/github.com/auroratechnologies/jsonconf).
---
+++ config.go
@@ -0,0 +1,49 @@
+package jsonconf
+
+import(
+	"os"
+	"log"
+	"fmt"
+	"errors"
+	"encoding/json"
+)
+
+/*
+Conf is a global variable that will map a given string to an interface generated from the JSON file.
+
+It will be loaded with values on initial LoadConfig call
+
+*/
+
+var Conf map[string]interface{}
+
+/*
+	Given a filename, LoadConfig will load a json file from the location. It will then decode it and store it into the global Conf variable
+*/
+
+func LoadConfig(filename string) {
+	file, _ := os.Open(filename)
+	decoder := json.NewDecoder(file)
+	var f interface{}
+	err := decoder.Decode(&f)
+	if err != nil {
+    log.Fatal(err)
+  }
+	Conf = f.(map[string]interface{})
+}
+
+/*
+	Given the name of a string, GetVar will try to locate an environment variable that matches the string. If that match comes empty, then it will look in the configuration file for the string as a key. If that match comes up empty, then the call will return an error. Otherwise, it will return the string of the interface{} stored in Conf
+*/
+
+func GetVar(v string) (string, error){
+	env := os.Getenv(v)
+	if (env != ""){
+		return env, nil
+	}
+	result, found := Conf[v]
+	if (!found){
+		return "", errors.New("Value not found")
+	}
+	return fmt.Sprintf("%v", result), nil
+}
