f0d6ea8bf77b2dd26705d68c25c44c627551aa63
Neville Jos <njos@Nevilles-MacBook-Pro.local>
2015-06-10 16:22:05 -0700

    Tests added

---
+++ config_test.go
@@ -0,0 +1,38 @@
+package jsonconf
+
+import "testing"
+
+func TestAll(t *testing.T) {
+	err := LoadConfig("fake.json")
+	if err == nil {
+		t.Error("Should return back an error for incorrect file")
+	}
+
+	err = LoadConfig("config_test.json")
+	if err != nil {
+		t.Error("Should load the file given")
+	}
+
+	v, err := GetVar("FOO")
+	if err != nil {
+		t.Error("Should return the value of FOO")
+	}
+
+	if v != "Hi" {
+		t.Error("Wrong value retried for FOO")
+	}
+
+	i, err := GetVar("BAR")
+	if err != nil {
+		t.Error("Should return the value of BAR")
+	}
+	if i != "1" {
+		t.Error("String conversion didn't work")
+	}
+
+	_, err = GetVar("FAKE")
+	if err == nil {
+		t.Error("Should return an error for an entry that doesn't exist")
+	}
+
+}
